3b5f3799fe7ed26a32074dd87febcd09d0c8ecc8
Radinal Saragih <rss@radinalsaragih@gmail.com>
2022-09-02 18:18:56 +0700

    initial commit

---
+++ qutebrowser/searchEngines.py
@@ -0,0 +1,43 @@
+c.url.searchengines = {
+        "DEFAULT": "https://lite.duckduckgo.com/lite/?q={}",
+
+        "dk" : "https://duckduckgo.com/?q={}",
+        "dkimg" : "https://duckduckgo.com/?q={}&kp=-2&iar=images&iax=images&ia=images",
+        "dkl": "https://lite.duckduckgo.com/lite/?q={}",
+        "sx" : "https://search.disroot.org/search?q={}",
+        "st" : "https://www.startpage.com/sp/search?query={}&language=english&t=device&lui=english&prfe=4363ad36d6a9caf46cb540f13bf9388813ce7ee8b40fe434a01febe8341f6ed40a662642663e197bfe48d294253fd681319486d663eb5f8e40be1cd469df0f3a123ec8eb8a266ba276f1f6b98540&cat=web&sc=XRGIfu8Mob3M20&abp=-1",
+        "br" : "https://search.brave.com/search?q={}&source=web",
+
+        "rd" : "https://libredd.it/search?q={}",
+        "tw" : "https://nitter.net/search?f=tweets&q={}&since=&until=&near=",
+        "tkt" : "https://proxitok.pussthecat.org/tag/{}",
+        "tku" : "https://proxitok.pussthecat.org/@{}",
+        "tkm" : "https://proxitok.pussthecat.org/music/{}",
+        "tkv" : "https://proxitok.pussthecat.org/@placeholder/video/{}",
+
+        "gt" : "https://github.com/search?q={}",
+        "tkp" : "https://www.tokopedia.com/search?st=product&q={}&srp_component_id=02.01.00.00&navsource=home",
+
+        "ar" : "https://archive.org/search.php?query={}",
+        "gt" : "https://www.gutenberg.org/ebooks/search/?query={}&submit_search=Go%21",
+        "pdfd" : "https://www.pdfdrive.com/search?q={}&pagecount=&pubyear=&searchin=&em=",
+        "gtcomic" : "https://getcomics.info/?s={}",
+
+        "od" : "https://odysee.com/$/search?q={}",
+        "yt" : "https://www.youtube.com/results?search_query={}",
+        "vd1" : "https://invidious.snopyta.org/search?q={}",
+        "vd2" : "https://inv.vern.cc/search?q={}",
+        "vd3" : "https://invidious.projectsegfau.lt/search?q={}",
+        "wh" : "https://wallhaven.cc/search?q={}",
+
+        "debpkg" : "https://tracker.debian.org/pkg/{}",
+        "ubuntupkg" : "https://packages.ubuntu.com/search?keywords={}",
+        "archpkg" : "https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged=",
+        "archaur" : "https://aur.archlinux.org/packages?O=0&K={}",
+
+        "wk" : "https://wikiless.org/w/index.php?search={}&title=Special%3ASearch&fulltext=Search",
+        "wkp" : "https://en.wikipedia.org/wiki/{}",
+        "aw" : "https://wiki.archlinux.org/?search={}",
+        "dw" : "https://wiki.debian.org/FrontPage?action=fullsearch&context=180&value={}&titlesearch=Titles",
+        "pcsx2w" : "https://wiki.pcsx2.net/index.php?search={}&title=Special%3ASearch&go=Go",
+}
